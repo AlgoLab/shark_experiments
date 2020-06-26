af0dd59dd7b55c1221ca88b6416d543c428b85c2
Luca Denti <ldenti92@gmail.com>
2020-06-26 17:29:43 +0200

    Minor fixes to genepanel exp

--- panelsize/combine.sh
+++ panelsize/combine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Run,P,R,time(s),RAM(GB)"
+echo "Run,P,R,time(s),RAM(MB)"
 for csv in $@
 do
     run=$(basename $csv .res)
@@ -9,5 +9,5 @@ do
     R=$(tail -1 $csv | cut -f 5)
     t=$(grep wall $time | cut -d' ' -f8 | awk -F: '{ if (NF == 1) {print $NF} else if (NF == 2) {print $1 * 60 + $2} else if (NF==3) {print $1 * 3600 + $2 * 60 + $3} }')
     ram=$(grep Maximum $time | cut -d' ' -f6)
-    echo $run,$P,$R,$t,$((ram/1024/1024))
+    echo $run,$P,$R,$t,$((ram/1024))
 done
